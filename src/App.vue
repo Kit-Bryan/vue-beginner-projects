2b884f12cfbe12924747f6ecbcbb08c2a61612d5
KitBryan <bryanwwk@gmail.com>
2023-08-13 20:50:47 +0800

    Added routerlink

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,15 @@
 <script setup>
-import q from "./data/quizes.json"
-import { ref, watch } from "vue";
-import Card from "@/components/Card.vue";
-
-const quizzes = ref(q);
-const search = ref("");
-
-watch(search, ()=> {
-    quizzes.value = q.filter((item)=> item.name.toLowerCase().includes(search.value.toLowerCase()))
-})
-
+import { RouterView } from "vue-router";
+import Nav from "@/components/Nav.vue";
 </script>
-
 <template>
-    <div class="container">
-        <header>
-            <h1>Quizzes</h1>
-            <input v-model.trim="search" type="text" name="" id="" placeholder="...">
-        </header>
-        <div class="options-container">
-            <Card v-for="quiz in quizzes" :key="quiz.id" :quiz="quiz"/>
-        </div>
+    <div>
+        <Nav/>
+        <RouterView/>
     </div>
 </template>
 
 <style scoped>
-.container {
-    /* Limit container to not stretch all the way to the sides of viewport */
-    max-width: 1000px;
-    /*center*/
-    margin: 0 auto;
-}
-
-header {
-    margin-bottom: 10px;
-    margin-top: 30px;
-    display: flex;
-    align-items: center;
-}
-
-header h1 {
-    font-weight: 600;
-    margin-right: 30px;
-}
-
-header input {
-    border: none;
-    background-color: rgba(128, 128, 128, 0.1);
-    padding: 10px;
-    border-radius: 5px;
-}
-
-.options-container {
-    display: flex;
-    flex-wrap: wrap;
-    margin-top: 40px;
-}
-
-
 
 </style>
+
---
+++ src/components/Nav.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+    <div>
+        <RouterLink active-class="active" to="/">Home</RouterLink>
+        <RouterLink active-class="active" to="/about">About</RouterLink>
+    </div>
+</template>
+
+<style scoped>
+    .active {
+        font-weight: 900;
+        color: rgba(235,210,31,0.7);
+    }
+</style>
